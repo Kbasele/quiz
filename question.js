7e77c11978f612cf51a3043acde2659093b35687
Kevin Basele <kevinbasele@hotmail.se>
2020-10-05 22:54:20 +0200

    added reset button

--- question.js
+++ question.js
@@ -22,37 +22,42 @@ class QuestionClass {
     setNewQuestion() {
         let shownQuestion = document.getElementById("shown-question");
         let shownAnswersUl = document.getElementById("shown-Answers");
+        let resetBtn = document.createElement("input")
+        resetBtn.type ="button"
+        resetBtn.value ="reset"
+
+        shownAnswersUl.appendChild(resetBtn)
+
         this.count++
 
         shownAnswersUl.innerHTML = ""
 
-        let anotherCount = 0; 
+        let anotherCount = 0;
+        
         for(let i = 0; i < Object.values(this.questionsArray[this.count].answers).length; ++i){
             if(Object.values(this.questionsArray[this.count].answers)[i] != null){
                 let newAnswers = document.createElement("li");
                 shownAnswersUl.appendChild(newAnswers)
+                shownAnswersUl.appendChild(resetBtn)
                 newAnswers.innerHTML = Object.values(this.questionsArray[this.count].answers)[i]
-
+                
                 let clickcount = 0; 
-                let answer; 
-
+                let answer;
+                let correct; 
+                
                 newAnswers.addEventListener("click", e => {
+                    //resetBtn.addEventListener("click", e =>{
+                    //    this.userAnswers[this.count-1].answer = []
+                    //    this.userAnswers[this.count-1].correct = []
+                    //    console.log("userAnswer")
+                    //    console.log(this.userAnswers[this.count-1].answer)
+                    //    console.log("correctAnwser")
+                    //    console.log( this.userAnswers[this.count-1].correct)                
+                    //})
                     ++clickcount
-                    if(clickcount == 2){
-                        newAnswers.style.backgroundColor = "rgba(182, 182, 182, 0.459)"
-                        clickcount = 0; 
-                        anotherCount =0; 
-
-                        for(let usersAnswer of this.userAnswers){
-                            if(usersAnswer == answer){
-                                let userAnswerIndex =  this.userAnswers.indexOf(answer)
-                                this.userAnswers.splice(userAnswerIndex, 1)
-                                this.userAnswers[this.count-1].answer.splice(1,1)
-                                console.log(this.userAnswers[this.count-1])
-                            }
-                        }                     
+                    if(clickcount > 1){
+                        console.log(this.userAnswers[this.count-1].answer)
                     }
-
                     else{
                         newAnswers.style.backgroundColor =  "rgba(65, 105, 225, 0.5)"
                         answer = {
@@ -63,27 +68,27 @@ class QuestionClass {
                         
                         this.userAnswers[this.count-1].answer.push(Object.values(this.questionsArray[this.count].correctAnswers)[i])
                         //this.userAnswers[0].correct.push(Object.values(this.questionsArray[this.count].correctAnswers)[i])
-
+                        
                         //console.log(this.userAnswers)
                         anotherCount++  
-
+                        
                         for(let answer of Object.values(this.questionsArray[this.count].correctAnswers)){
                             console.log(anotherCount)
                             if(answer == "true" && anotherCount < 2 ){ 
                                 this.userAnswers[this.count-1].correct.push(answer)
                             }
                         }
-                        //console.log("userAnswer")
-                        //console.log(this.userAnswers[this.count-1].answer)
-                        //console.log("correctAnwser")
-                        //console.log( this.userAnswers[this.count-1].correct)
+                        
+                        console.log("userAnswer")
+                        console.log(this.userAnswers[this.count-1].answer)
+                        console.log("correctAnwser")
+                        console.log( this.userAnswers[this.count-1].correct)
                     }
                 })
             }
         }
         shownQuestion.innerHTML = this.questionsArray[this.count].question;
     }
-    
 
     correctUserAnswer(correctAnswer, userAnwser){
     }
